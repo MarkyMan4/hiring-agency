e20300695b846efcd014369be69f52e681682ba3
MarkyMan4 <markus.hutnik@gmail.com>
2022-01-30 17:38:36 -0600

    removed secret key from settings.py, configured connection for postgresql

--- hiring_agency/settings.py
+++ hiring_agency/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import json
+
+# load secrets file
+with open('secrets.json') as f:
+    secrets = json.load(f)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j4**%*gyehept0rlvfo5uxkc1=kd)=*2+fqr7)d+=9h9+e1sc)'
+SECRET_KEY = secrets['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,10 +88,21 @@ CORS_ORIGIN_WHITELIST = [
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': secrets['db_name'],
+        'USER': secrets['user'],
+        'PASSWORD': secrets['password'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
