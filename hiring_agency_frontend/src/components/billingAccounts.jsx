484e6996e4a11a44d1e9c5d2039c9b57003703cf
MarkyMan4 <markus.hutnik@gmail.com>
2022-03-04 22:51:54 -0600

    created placeholder page for viewing billing accounts

---
+++ hiring_agency_frontend/src/components/billingAccounts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BillingAccounts() {
+    return (
+        <div>
+            <h1>billing accounts</h1>
+        </div>
+    );
+}
+
+export default BillingAccounts;
